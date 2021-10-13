c00ba166e1fd7de634afe01fbfa6e5eeedb18484
bayazid_sustami <bayazidsustamy@gmail.com>
2021-10-13 18:58:52 +0800

    compare obj

---
+++ oop/comparing_obj/ComparingObject.php
@@ -0,0 +1,16 @@
+<?php
+
+require_once("../object_cloning/Student.php");
+
+$student1 = new Student();
+$student1->id = "1";
+$student1->name = "bay";
+$student1->grade = 3;
+
+$student2 = new Student();
+$student2->id = "1";
+$student2->name = "bay";
+$student2->grade = 3;
+
+var_dump($student1 == $student2); //comparing all properties
+var_dump($student1 === $student2); //comparing with reference
