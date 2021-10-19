0c9f6a8f06a4be85d12233e72c35ff32bba3d2d1
bayazid_sustami <bayazidsustamy@gmail.com>
2021-10-19 14:09:40 +0800

    user login and implement middleware

---
+++ laravel/first-app/resources/views/dashboard/index.blade.php
@@ -0,0 +1,1 @@
+<h1>Welcome boys</h1>
--- laravel/first-app/routes/web.php
+++ laravel/first-app/routes/web.php
@@ -3,6 +3,7 @@
 use App\Models\Category;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BlogController;
+use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\RegisterController;
 
@@ -49,6 +50,10 @@ Route::get('/categories', function () {
     ]);
 });
 
-Route::get('/login', [LoginController::class, 'index']);
-Route::get('/register', [RegisterController::class, 'index']);
+Route::get('/login', [LoginController::class, 'index'])->name('login')->middleware('guest');
+Route::post('/login', [LoginController::class, 'authenticate']);
+Route::post('/logout', [LoginController::class, 'logout']);
+Route::get('/register', [RegisterController::class, 'index'])->middleware('guest');
 Route::post('/register', [RegisterController::class, 'store']);
+
+Route::get('/dashboard', [DashboardController::class, 'index'])->middleware('auth');
